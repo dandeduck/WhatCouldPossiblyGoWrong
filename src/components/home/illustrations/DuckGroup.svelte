48841aa7f7cf6c2eef9b6affbbfedd30e40b9586
Daniel Mikhailov <danielmkhlv@gmail.com>
2023-06-14 21:56:47 +0300

    Uncut duck legs

--- src/components/home/illustrations/DuckGroup.svelte
+++ src/components/home/illustrations/DuckGroup.svelte
@@ -4,7 +4,7 @@
     import ScarfDuck from '../../icons/ScarfDuck.svelte'
 </script>
 
-<div class="flex mt-3 pb-5 justify-center items-end overflow-hidden h-44">
+<div class="flex mt-3 pb-5 justify-center items-end h-44">
     <BowtieDuck className="scale-150 mr-10" />
     <CapDuck className="scale-150 mr-5" />
     <ScarfDuck className="scale-150" />
